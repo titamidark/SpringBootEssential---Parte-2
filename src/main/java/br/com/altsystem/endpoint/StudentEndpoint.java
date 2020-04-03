9a9d7867dddb1553b648cac42ecbdbec53c5505f
musta <altcaeol@gmail.com>
2020-04-03 11:08:00 -0300

    Projeto 5 - Padroes REST e POST Parte 1

--- src/main/java/br/com/altsystem/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/altsystem/endpoint/StudentEndpoint.java
@@ -1,27 +1,41 @@
 package br.com.altsystem.endpoint;
 
+import br.com.altsystem.error.CustomErrorType;
 import br.com.altsystem.model.Student;
 import br.com.altsystem.utils.DateUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("students")
 public class StudentEndpoint {
 
-    @Autowired
-    private DateUtil dateUtil;
+    private final DateUtil dateUtil;
 
-    @RequestMapping(method = RequestMethod.GET, path = "/list")
-    public List<Student> listAll() {
-        System.out.println("xxxxxxxxxxx" + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()) + "xxxxxxxxxxx");
-        return asList(new Student("Altamir Caetano"), new Student("Mustafá Ahimed"));
+    public StudentEndpoint(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> listAll() {
+        System.out.println("xxxxsxxxxxxx" + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()) + "xxxxxxxxxxx");
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+
+        if (index == -1)
+            return new ResponseEntity<>(new CustomErrorType("Student NOT FOUND!"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
 }
--- src/main/java/br/com/altsystem/model/Student.java
+++ src/main/java/br/com/altsystem/model/Student.java
@@ -1,14 +1,64 @@
 package br.com.altsystem.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static java.util.Arrays.asList;
+
 public class Student {
 
+    private int id;
     private String name;
+    public static List<Student> studentList;
+
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Student(String name) {
         this.name = name;
     }
 
     public Student() {
+    }
+
+    private static void studentRepository() {
+        studentList = new ArrayList<>(asList(new Student(23,"Altamir Caetano"), new Student(33,"Mustafá Ahimed")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public static void setStudentList(List<Student> studentList) {
+        Student.studentList = studentList;
     }
 
     public String getName() {
